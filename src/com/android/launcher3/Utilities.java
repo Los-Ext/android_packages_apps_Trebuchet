6701dafbff01b22a9d9227acfedde9c191c8af48
minaripenguin <minaripenguin@users.noreply.github.com>
2025-01-22 07:51:57 +0800

    Disable recents blur by default
    * live tile have some issues with blur, to avoid UI glitches unless the user adjusts the blur level, set the blur to 0 by default, follows NOS 3.0 UI
    
    test: compare with NOS 3.0, check for live tile glitches

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -1128,7 +1128,7 @@ public final class Utilities {
    }
 
     public static int getRecentsOpacity(Context context) {
-        return 0;
+        return 50;
     }
 
     public static int getAllAppsOpacity(Context context) {
@@ -1137,8 +1137,7 @@ public final class Utilities {
 
     public static int getBlurRadius(Context context) {
         SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
-        return prefs.getInt(KEY_BLUR_DEPTH,
-                (int) context.getResources().getDimension(R.dimen.max_depth_blur_radius));
+        return prefs.getInt(KEY_BLUR_DEPTH, 0);
     }
 
     public static boolean isShortParallax(Context context) {
